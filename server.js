cd0aebdaea09e13387867c19aab1a259221d90b7
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-23 17:49:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function(req,res){
+    res.send("Article one is requested and will be served");
+});
+
+app.get('/article-two', function(req,res){
+    res.send("Article two is requested and will be served");
+});
+
+app.get('/article-three', function(req,res){
+    res.send("Article three is requested and will be served");
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
