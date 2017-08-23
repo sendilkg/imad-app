b84a251331f778bb6a4fdaf8523197191bae989e
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-23 18:44:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Sendil',
+    heading: 'Article One',
+    date: 'August 23, 2017',
+    content:` 
+    <p> this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.
+    </p>
+    <p> this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.
+    </p>
+    <p> this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.this is my article one.
+    </p>
+    `
+    
+}
+var displayHtml= function(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date= data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-sacle=1" />
+            <link href="ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href='\'> Home</a>
+                </div>
+                <hr>
+                <h3> 
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}        
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return(htmlTemplate);
+};
+
+
 app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
+   res.send(displayHtml(articleOne));
 });
 
 app.get('/article-two', function(req,res){
