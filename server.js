c7578619426e55172bd517d02ab56aa042137122
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-29 17:21:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,14 @@ app.get('/counter', function(req,res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res){
+    var name = req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
 app.get('/:articleFiled', function(req,res){
     var articleName = req.params.articleFiled;
     res.send(displayHtml(articles[articleName]));
