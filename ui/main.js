2ecd256a19204c0f039e29abc1b2486869a7be5d
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-29 15:51:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,15 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft = '10px';
 
-}
+}*/
+
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementbyId('count');
+    span.innerHtml = counter.toString();
+    
+};
