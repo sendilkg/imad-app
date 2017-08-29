b9e9e53fc18b0aeba169c9866d053b68cfa5c73c
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-29 17:30:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,12 +42,32 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to the server and send the name
     //capyture the list of names and render it to the list
-    var names = ["name1", "name2", "name3"];
-    var list = '';
-    for(var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+        //create a request variable
+    var request = new XMLHttpRequest();
+    
+    //process the request info
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take action
+            if(request.status === 200 ){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i< names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+           
+            //not done yet
+        }
+        
+    };
+    //make a request
+    request.open('GET', 'http://sendilcareer.imad.hasura-app.io/submit-name?name='+ nameValue, true);
+    request.send(null);
+    
 };
 
