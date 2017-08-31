739f8fcdc805c912632ed54eea9e08e741adeb8f
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-31 22:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,8 +150,6 @@ app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
     
-    
-    var dbString = hash(password,salt);
     pool.query('SELECT * FROM users WHERE username=$1',[username],function(err, result){
         if(err){
              res.status(500).send(err.toString());
