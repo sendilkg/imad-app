cf6277a959f3da9e54c80945f10101be998cbf74
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-31 21:03:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/counter', function(req,res){
 function hash(input, salt)
 {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return(hashed.toString('hex'));
+    return["pbkdf2","10000","This-is-a-random-string",hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function(req, res){
     
