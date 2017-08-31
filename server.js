9dd51e7d0c4d756dd02bcdb9235ad3b3a1c01ecd
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-31 16:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/test-db', function(req, res){
         }
        else
        {
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
     })
     
