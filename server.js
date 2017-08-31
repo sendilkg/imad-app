e118fbbb2082db46cb0b9695991c68a6f3c7bb0a
sendilkg <31275444+sendilkg@users.noreply.github.com>
2017-08-31 16:18:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,24 @@ app.get('/test-db', function(req, res){
     
 });
 
+app.get('articles/:articleFiled', function(req,res){
+    pool.query("SELECT * FROM articles WHERE title=" + req.params.articleFiled, function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length === 0 ){
+                 res.status(404).send("Article not found");
+            }
+            else{
+                var articleData = result.rows[0];
+                 res.send(displayHtml(articleData));
+            }
+        }
+    });
+   
+});
+
 var names = [];
 app.get('/submit-name', function(req, res){
     var name = req.query.name;
